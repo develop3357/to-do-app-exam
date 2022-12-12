d4c3187653d97b5aa2af1aa85da4138b951eabfd
Jihyeon Kim <develop3357@gmail.com>
2022-12-12 13:18:24 +0900

    from lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { createGlobalStyle } from "styled-components";
 import { Reset } from "styled-reset";
+import ToDoList from "./components/ToDoList";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+}
+`;
 
 function App() {
   return (
-    <div>
+    <>
       <Reset />
-    </div>
+      <GlobalStyle />
+      <ToDoList />
+    </>
   );
 }
 
---
+++ src/components/atoms.ts
@@ -0,0 +1,49 @@
+import { atom, selector } from "recoil";
+import { RecoilValueReadOnly } from "recoil";
+
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
+export interface IToDo {
+  text: string;
+  id: number;
+  category: Categories;
+}
+
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: any, _: any, isReset: any) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
+
+export const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+  effects: [localStorageEffect("toDo")],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
+});
